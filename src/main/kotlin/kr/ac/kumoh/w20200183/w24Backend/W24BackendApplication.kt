1e8989530bc9a819be6a5374436083db76cadf61
makerel0818 <minji2700@naver.com>
2024-12-12 13:32:43 +0900

    🎉 Term Project Backend 시작

---
+++ src/main/kotlin/kr/ac/kumoh/w20200183/w24Backend/W24BackendApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.w20200183.w24Backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class W24BackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<W24BackendApplication>(*args)
+}
---
+++ src/test/kotlin/kr/ac/kumoh/w20200183/w24Backend/W24BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.w20200183.w24Backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class W24BackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
